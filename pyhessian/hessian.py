Fix a minor mistake in hessian.py
- take the real parts of the eigenvalues from Lanczos.
- compute the square of only the first component of each eigenvector as density.

pyhessian/hessian.py
@@ -260,8 +260,8 @@ class hessian():
                     T[i, i + 1] = beta_list[i]
             a_, b_ = torch.linalg.eig(T)
 
-            eigen_list = a_
-            weight_list = torch.pow(b_, 2)
+            eigen_list = a_.real
+            weight_list = torch.pow(b_[0,:], 2)
             eigen_list_full.append(list(eigen_list.cpu().numpy()))
             weight_list_full.append(list(weight_list.cpu().numpy()))
 